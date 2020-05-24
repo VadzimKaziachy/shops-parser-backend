1e44dc4114eb93fd5946e5ca3fc45d679d549c14
Vadim Koziachiy <vadim.koziachiy@exposit.com>
2020-05-24 20:12:17 +0300

    fix - minor fix.

--- src/home/managers.py
+++ src/home/managers.py
@@ -5,5 +5,5 @@ from django.shortcuts import get_object_or_404
 
 
 class HomeManager(models.Manager):
-    def get_first_model_to_dict(self):
+    def get_first_object_to_dict(self):
         return model_to_dict(get_object_or_404(self.get_queryset()))
--- src/home/views.py
+++ src/home/views.py
@@ -7,4 +7,4 @@ class HomeView(generic.TemplateView):
     template_name = 'home/home_page.html'
 
     def get_context_data(self, **kwargs):
-        return HomeModel.objects.get_first_model_to_dict()
+        return HomeModel.objects.get_first_object_to_dict()
